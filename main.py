52e31a12991111f52f48b3c8ef6dcfc673ce492a
kawt0608 <kaw683129@gmail.com>
2025-02-22 05:56:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# main.py
+import sys
+from PySide6.QtWidgets import QApplication
+from stopwatch import Stopwatch
 
+def main():
+    app = QApplication(sys.argv)
+    stopwatch = Stopwatch()
+    stopwatch.show()
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
